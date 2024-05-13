599b452c9908e0c905e41770ff8d722cad05296f
RadonZ <supachai27102000@gmail.com>
2024-05-13 23:28:42 +0700

    finish middleware

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARE
+if(process.env.NODE_ENV === 'development') {
 app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
@@ -20,10 +23,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 })
-
-
-
-
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
