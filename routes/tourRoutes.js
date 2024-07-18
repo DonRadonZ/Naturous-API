5d11667fcbcf7a9e5182692599ebb0eef5ded6c2
RadonZ <supachai27102000@gmail.com>
2024-07-18 16:12:46 +0700

    add: pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,7 +12,17 @@ const router = express.Router();
 
 // Add it to the post handler stack
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router
+    .route('/top-5-cheap')
+    .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
+    .route('/tour-stats')
+    .get(tourController.getTourStats);
+
+router
+    .route('/monthly-plan/:year')
+    .get(tourController.getMonthlyPlan);
 
 router
     .route('/')
