5858f89b1660f1c7a8f5a0ffa6709ee0d66fab7e
RadonZ <supachai27102000@gmail.com>
2024-07-25 10:24:57 +0700

    add: error handling

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
 
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -26,10 +25,26 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*',(req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  })
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next();
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
 });
 
 // 4) START SERVER
