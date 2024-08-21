dd5d8291db61c0b85087d94748657af8a2d9bf24
RadonZ <supachai27102000@gmail.com>
2024-08-21 15:19:34 +0700

    update: add calculate distance

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -112,7 +112,7 @@ export const getToursWithin = catchAsync(async (req, res, next) => {
     const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
 
     if(!lat || !lng){
-        next(AppError('Please provide latitude and longitude in the format lat,lng.', 404))
+        next(AppError('Please provide latitude and longitude in the format lat,lng.', 400))
     }
 
     const tours = await Tour.find({ startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } } });
@@ -123,5 +123,42 @@ export const getToursWithin = catchAsync(async (req, res, next) => {
         data: {
             data: tours
         }
-    })
+    });
+})
+
+export const getDistances = catchAsync(async(req, res, next) => {
+    const { latlng, unit } = req.params;
+    const [lat, lng] = latlng.split(',');
+
+    const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+    if(!lat || !lng){
+        next(AppError('Please provide latitude and longitude in the format lat,lng.', 400))
+    }
+
+    const distances = Tour.aggregate([
+        {
+            $geoNear: {
+              near: {
+                type: 'Point',
+                coordinates: [lng * 1, lat * 1]
+              },  
+              distanceField: 'distance',
+              distanceMultiplier: multiplier
+            }
+        },
+        {
+            $project: {
+                distance: 1,
+                name: 1
+            }
+        }
+    ]);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            data: distances
+        }
+    });
 })
--- server/routes/tourRoutes.js
+++ server/routes/tourRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { aliasTopTours, getAllTours, getTourStats, getMonthlyPlan, createTour, getTour, updateTour, deleteTour, getToursWithin } from '../controllers/tourController.js';
+import { aliasTopTours, getAllTours, getTourStats, getMonthlyPlan, createTour, getTour, updateTour, deleteTour, getToursWithin, getDistances } from '../controllers/tourController.js';
 import { protect, restrictTo } from '../controllers/authController.js';
 import reviewRouter from './reviewRoutes.js';
 
@@ -35,6 +35,8 @@ router
     .get(getToursWithin)
 // /tours-distance?distance=233&center=-40,45&unit=mi
 // /tours-distance/233
+
+router.route('distances/:latlng/:unit').get(getDistances)
 
 router
     .route('/')
